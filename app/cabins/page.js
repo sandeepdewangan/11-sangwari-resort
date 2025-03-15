d500166eb1c349175a479568a559ea6f7366b39d
Sandeep <sandeepdewangan7@gmail.com>
2025-03-15 14:12:09 +0530

    Data Fetching

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-const page = () => {
+const page = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+
   return (
     <div>
       <h1>Cabin Page</h1>
+      {data.map((d) => (
+        <p>{d.title}</p>
+      ))}
     </div>
   );
 };
